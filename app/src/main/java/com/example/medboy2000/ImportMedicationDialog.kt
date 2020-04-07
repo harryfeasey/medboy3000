69439f2b992dcc950bcc4965c57d9db1f2ae9b8f
Harry Feasey <hef25@uclive.ac.nz>
2020-04-07 21:13:50 +1200

    Beginning to store data to file. Need to load into cards. detects which day it is.

--- app/src/main/java/com/example/medboy2000/ImportMedicationDialog.kt
+++ app/src/main/java/com/example/medboy2000/ImportMedicationDialog.kt
@@ -1,19 +1,18 @@
 package com.example.medboy2000
 
 import android.app.TimePickerDialog
+import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.os.Build
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.importmedication.*
+import java.io.*
 import java.util.*
 
 class ImportMedicationDialog (private val callbackListener: CallbackListener) : DialogFragment() {
@@ -31,21 +30,62 @@ class ImportMedicationDialog (private val callbackListener: CallbackListener) : 
     }
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
+
+
         saveButton.setOnClickListener {
             //send back data to PARENT fragment using callback
-            callbackListener.onDataReceived(editText.text.toString())
+            callbackListener.onDataReceived(nameTxt.text.toString())
+
+            // --------------------------------
+
+
+            val name:String = nameTxt.text.toString()
+            val dosage:String = dosageTxt.text.toString()
+            val weekly:String = weeklyButton.isChecked.toString()
+            val fileOutputStream: FileOutputStream
+
+            try {
+
+                fileOutputStream = context!!.openFileOutput("day", Context.MODE_PRIVATE)
+                fileOutputStream.write(name.toByteArray())
+                fileOutputStream.write(dosage.toByteArray())
+                fileOutputStream.write(weekly.toByteArray())
+
+                //TODO create object, and convert to byte array.
+
+            } catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }catch (e: NumberFormatException){
+                e.printStackTrace()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            Toast.makeText(context!!.applicationContext,"Medication added.", Toast.LENGTH_LONG).show()
+
+
+        // --------------------------------
+
+
             // Now dismiss the fragment
             dismiss()
         }
+
 
         closeButton.setOnClickListener{
             dismiss()
         }
 
+
+
+        // -------------------------- REMINDER TIME PICKER --------------------------
         val calendar = Calendar.getInstance()
         val hour = calendar.get(Calendar.HOUR) + 1
         val min = calendar.get(Calendar.MINUTE)
+
         calendar.set(Calendar.HOUR, hour)
         calendar.set(Calendar.MINUTE, min)
 
@@ -64,6 +104,11 @@ class ImportMedicationDialog (private val callbackListener: CallbackListener) : 
                 context, timeSetListener, calendar.get(Calendar.HOUR),
                 calendar.get(Calendar.MINUTE), false).show()
         }
+
+        // -----------------------------------------------------------------------------
+
+
+
 
 
     }
--- app/src/main/java/com/example/medboy2000/MainActivity.kt
+++ app/src/main/java/com/example/medboy2000/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.medboy2000
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(), CallbackListener {
 
@@ -10,7 +13,7 @@ class MainActivity : AppCompatActivity(), CallbackListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         floating_action_button.setOnClickListener { showDialog()}
-
+        loadDayOfWeek()
     }
 
     private fun showDialog() {
@@ -21,6 +24,26 @@ class MainActivity : AppCompatActivity(), CallbackListener {
 
     override fun onDataReceived(data: String) {
         textView.text = data
+    }
+
+    private fun loadDayOfWeek(){
+        //TODO get day of week and select button on load.
+        val calendar = Calendar.getInstance()
+        val day = calendar.get(Calendar.DAY_OF_WEEK)
+        var pageButton = when (day){
+            1 -> monButton
+            2 -> tueButton
+            3 -> wedButton
+            4 -> thuButton
+            5 -> friButton
+            6 -> satButton
+            else -> sunButton
+        }
+
+        pageButton.isSelected = true
+        pageButton.setBackgroundColor(Color.parseColor("#8E56DF"))
+
+
     }
 
 }
